65e9ff40158f8e990fdd8ca5f9c29ad2b9e3eb3f
zpdwo <zhangpengdong@ctsi.com.cn>
2017-07-31 16:32:29 +0800

    笔记

---
+++ src/main/java/zpd/zhujie/CreatePreamble.java
@@ -0,0 +1,13 @@
+package zpd.zhujie;
+
+import java.lang.annotation.Documented;
+
+/**
+ * Created by Ada on 2017-7-31.
+ */
+@Documented // 为了让信息显示在Javadoc生成的文档。
+
+public @interface CreatePreamble {
+
+  String test1();
+}
---
+++ src/main/java/zpd/zhujie/Test.java
@@ -0,0 +1,15 @@
+package zpd.zhujie;
+
+/**
+ * 注解测试.
+ *
+ * Created by Ada on 2017-7-31.
+ */
+@CreatePreamble(test1 = "123")
+public class Test {
+
+  private static void test1() {
+
+  }
+
+}
